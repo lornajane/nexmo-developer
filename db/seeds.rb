b7251ac3bec2249b2f06a8130ed0230cf78f32ec
Adam Butler <adam@lab.io>
2018-05-01 16:38:06 +0100

    Change create -> create!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create!(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@nexmo.com', password: 'development', admin: true)
+User.create!(email: 'admin@nexmo.com', password: 'development', admin: true)
 
 Event.create!({
   title: 'Small business hackathon',
