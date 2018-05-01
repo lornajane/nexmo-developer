21490569a010af0e055022f6cbb09b1b5ed15c30
Adam Butler <adam@lab.io>
2018-05-01 17:01:00 +0100

    Do not create admin user in test environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create!(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'admin@nexmo.com', password: 'development', admin: true)
+unless Rails.env.test?
+  User.create!(email: 'admin@nexmo.com', password: 'development', admin: true)
+end
 
 Event.create!({
   title: 'Small business hackathon',
